67575260bf62ffc33cb133d7a3cffa258d3ae903
Fadeyev20 <fda2000@ua.fm>
2021-04-10 19:58:30 +0300

    Подменяет значение атрибута src элемента img.lightbox__image.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import gallery_items from '../gallery-items.js';
 
 const listEl = document.querySelector('.js-gallery');
 const modalEl = document.querySelector('.js-lightbox');
+const modalImgEl = modalEl.querySelector('img');
 
 const galleryEls = gallery_items.map(({ preview, original, description }) => {
     return `
@@ -30,5 +31,7 @@ function onGalleryItemClick(e) {
     if (e.target.nodeName !== 'IMG') return;
 
     modalEl.classList.add('is-open');
+    modalImgEl.src = e.target.dataset.source;
+    modalImgEl.alt = e.target.alt;
 }
 
